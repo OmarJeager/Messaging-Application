e1538a45c023205489badb6e67c854c93b299d3d
OmarJeager <stirnermax45@gmail.com>
2024-10-01 12:25:35 +0100

    Messaging Application

--- home.php
+++ home.php
@@ -135,19 +135,10 @@
          	   });
        });
 
-
-      /** 
-      auto update last seen 
-      for logged in user
-      **/
       let lastSeenUpdate = function(){
       	$.get("app/ajax/update_last_seen.php");
       }
       lastSeenUpdate();
-      /** 
-      auto update last seen 
-      every 10 sec
-      **/
       setInterval(lastSeenUpdate, 10000);
 
     });
